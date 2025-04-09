e00def4b80e4a6dbd138d48ebbf2a24e7a27a2c9
brandencall <brandencall@live.com>
2025-04-09 14:22:55 -0600

    Added some animations to the PurpleKnightPlayer

--- scripts/player/PurpleKnightPlayer.cs
+++ scripts/player/PurpleKnightPlayer.cs
@@ -4,6 +4,9 @@ public partial class PurpleKnightPlayer : CharacterBody2D
 {
     [Export]
     private HealthComponent _healthComponent;
+    [Export]
+    public AnimatedSprite2D Animations { get; set; }
+
     public float Speed = 200;
 
     public override void _Ready()
@@ -16,6 +19,26 @@ public partial class PurpleKnightPlayer : CharacterBody2D
     {
         Vector2 inputDirection = Input.GetVector("move_left", "move_right", "move_up", "move_down");
         Velocity = inputDirection * Speed;
+        if (inputDirection.X != 0)
+        {
+            if (inputDirection.X < 0)
+            {
+                Animations.FlipH = true;
+            }
+            else 
+            {
+                Animations.FlipH = false;
+            }
+        }
+
+        if (Velocity == Vector2.Zero)
+        {
+            Animations.Play("idle");
+        }
+        else if (Velocity != Vector2.Zero)
+        {
+            Animations.Play("walk");
+        }
 
         MoveAndSlide();
     }
