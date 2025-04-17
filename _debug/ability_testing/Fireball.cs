189439691bcac3eead7a18768ae779545a6d7595
brandencall <brandencall@live.com>
2025-04-17 16:32:58 -0600

    Added Fireball ability for testing purposes to see how scene based ability setup would work.

---
+++ _debug/ability_testing/Fireball.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public partial class Fireball : Node2D
+{
+    [Export] public float Speed = 400f;
+    [Export] public Vector2 Direction = Vector2.Right;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        Position += Direction.Normalized() * Speed * (float)delta;
+    }
+
+    private void OnAreaEntered(Area2D area)
+    {
+        GD.Print("Fireball hit: ", area.Name);
+    }
+
+    public override void _Ready()
+    {
+        GetNode<Area2D>("Area2D").AreaEntered += OnAreaEntered;
+    }
+}
