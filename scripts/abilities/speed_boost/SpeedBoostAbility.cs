5aab83f7bef429955daaada83a8cff98a8b7d65b
brandencall <brandencall@live.com>
2025-04-24 12:09:58 -0600

    Added levels to speed boost ability.

--- scripts/abilities/speed_boost/SpeedBoostAbility.cs
+++ scripts/abilities/speed_boost/SpeedBoostAbility.cs
@@ -1,12 +1,74 @@
-using Godot;
 
-public partial class SpeedBoostAbility : AbilityLogic 
+public partial class SpeedBoostAbility : AbilityLogic
 {
-    [Export]
-    public float SpeedIncrease = 2;
+    private BasePlayer _player;
 
     public override void Apply(BasePlayer player)
     {
-        player.VelocityComponent.MaxSpeed *= 2;
+        _player = player;
+        player.VelocityComponent.MaxSpeed += player.VelocityComponent.MaxSpeed * 0.2f;
+    }
+
+    public override void Upgrade(AbilityResource abilityResource)
+    {
+        UpgradeBasedOnLevel();
+        base.Upgrade(abilityResource);
+        SetupNextLevelAbilityResource(abilityResource);
+    }
+
+    private void UpgradeBasedOnLevel()
+    {
+        switch (CurrentLevel)
+        {
+            case 2:
+                _player.VelocityComponent.MaxSpeed += _player.VelocityComponent.MaxSpeed * 0.2f;
+                break;
+            case 3:
+                _player.VelocityComponent.MaxSpeed += _player.VelocityComponent.MaxSpeed * 0.2f;
+                break;
+            case 4:
+                _player.VelocityComponent.MaxSpeed += _player.VelocityComponent.MaxSpeed * 0.2f;
+                break;
+            case 5:
+                _player.VelocityComponent.MaxSpeed += _player.VelocityComponent.MaxSpeed * 0.2f;
+                break;
+            case 6:
+                _player.VelocityComponent.MaxSpeed += _player.VelocityComponent.MaxSpeed * 0.2f;
+                break;
+            case 7:
+                _player.VelocityComponent.MaxSpeed += _player.VelocityComponent.MaxSpeed * 0.2f;
+                break;
+            case 8:
+                _player.VelocityComponent.MaxSpeed += _player.VelocityComponent.MaxSpeed * 0.2f;
+                break;
+        }
+    }
+
+    private void SetupNextLevelAbilityResource(AbilityResource abilityResource)
+    {
+        switch (CurrentLevel)
+        {
+            case 2:
+                abilityResource.Description = "+20% speed increase!";
+                break;
+            case 3:
+                abilityResource.Description = "+20% speed increase!";
+                break;
+            case 4:
+                abilityResource.Description = "+20% speed increase!";
+                break;
+            case 5:
+                abilityResource.Description = "+20% speed increase!";
+                break;
+            case 6:
+                abilityResource.Description = "+20% speed increase!";
+                break;
+            case 7:
+                abilityResource.Description = "+20% speed increase!";
+                break;
+            case 8:
+                abilityResource.Description = "+20% speed increase!";
+                break;
+        }
     }
 }
