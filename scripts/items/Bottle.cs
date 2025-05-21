5a0bd4c3c4e9a61d07f614ef59c539ff425d28c5
Branden Call <brandencall@live.com>
2025-05-21 13:51:34 -0600

    Bottle spins in the air now

--- scripts/items/Bottle.cs
+++ scripts/items/Bottle.cs
@@ -11,6 +11,8 @@ public partial class Bottle : HitboxComponent
 	public float Speed = 700f;
 	[Export]
 	public float LifeTime = 2.0f;
+	[Export]
+	public float RotationSpeed = 180.0f;
 
 	private BaseCharacter _character;
 	private Vector2 _direction;
@@ -23,8 +25,6 @@ public partial class Bottle : HitboxComponent
 		this.AreaEntered += OnAreaEntered;
 		GlobalPosition = _character.GlobalPosition;
 		_direction = _character.VelocityComponent.LastMoveDirection.Normalized();
-		Sprite.Rotation = _direction.Angle();
-		Collision.Rotation = _direction.Angle() - Mathf.Pi / 2f;
 		_currentSpeed = Speed;
 
 		Timer lifeTimer = new Timer();
@@ -36,7 +36,15 @@ public partial class Bottle : HitboxComponent
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Position += _direction.Normalized() * _currentSpeed * (float)delta;
+		Position += _direction * _currentSpeed * (float)delta;
+		if (_direction.X > 0)
+		{
+			Rotation += Mathf.DegToRad(RotationSpeed) * (float)delta;
+		}
+		else 
+		{
+			Rotation += Mathf.DegToRad(-RotationSpeed) * (float)delta;
+		}
 	}
 
 	private void OnAreaEntered(Area2D otherArea)
