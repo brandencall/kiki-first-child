22fee88ab83432858569260dd64a141a3a45190b
Branden Call <brandencall@live.com>
2025-05-28 11:57:47 -0600

    New rat enemy

---
+++ scripts/enemies/types/Rat.cs
@@ -0,0 +1,24 @@
+using Godot;
+
+public partial class Rat : BaseEnemy 
+{
+	[Export]
+	private VelocityComponent _velocityComponent;
+	[Export]
+	private PathfindComponent _pathfindComponent;
+	[Export]
+	private HealthComponent _healthComponent;
+
+	public override void _Ready()
+	{
+		base._Ready();
+		_healthComponent.Died += Die;
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		_pathfindComponent.FollowPath(GlobalPosition);
+		_velocityComponent.Move(this);
+	}
+
+}
