ffa74ae2c2df4c70516fe9a473183744f4ef1bca
Branden Call <brandencall@live.com>
2025-08-20 18:14:22 -0600

    Character card change

--- scripts/ui/CharacterCard.cs
+++ scripts/ui/CharacterCard.cs
@@ -3,83 +3,74 @@ using System;
 
 public partial class CharacterCard : Control
 {
-	[Export]
-	public TextureButton CharacterButton { get; set; }
-	[Export]
-	public Label CharacterName { get; set; }
-	[Export]
-	public Button BuyButton { get; set; }
+    [Export]
+    public Button CharacterButton { get; set; }
+    [Export]
+    public TextureRect CharacterSprite { get; set; }
+    [Export]
+    public Label CharacterName { get; set; }
+    [Export]
+    public Button BuyButton { get; set; }
 
-	public event Action<CharacterData> CharacterSelected;
+    public event Action<CharacterData> CharacterSelected;
 
-	private CharacterData _character;
-	private GameManager GameManager => GetNode<GameManager>("/root/GameManager");
+    private CharacterData _character;
+    private GameManager GameManager => GetNode<GameManager>("/root/GameManager");
 
-	public override void _Ready()
-	{
-		CharacterButton.Pressed += OnCharacterSelected;
-		BuyButton.Pressed += OnBuyButtonPressed;
-	}
+    public override void _Ready()
+    {
+        CharacterButton.Pressed += OnCharacterSelected;
+        BuyButton.Pressed += OnBuyButtonPressed;
+    }
 
-	public void UpdateCardInfo(CharacterData characterData)
-	{
-		SetCharacter(characterData);
-	}
+    public void UpdateCardInfo(CharacterData characterData)
+    {
+        SetCharacter(characterData);
+    }
 
-	private void OnCharacterSelected()
-	{
-		CharacterSelected?.Invoke(_character);
-	}
+    private void OnCharacterSelected()
+    {
+        if (_character.IsUnlocked)
+        {
+            CharacterSelected?.Invoke(_character);
+        }
+    }
 
-	private void OnBuyButtonPressed()
-	{
-		if (_character.Cost < GameManager.CurrencyBalance())
-		{
-			GameManager.WithdrawCurrency(_character.Cost);
-			GameManager.UnlockCharacter(_character.Id);
-			//_character.IsUnlocked = true;
-			OnCharacterSelected();
-		}
-	}
+    private void OnBuyButtonPressed()
+    {
+        if (_character.Cost < GameManager.CurrencyBalance())
+        {
+            GameManager.WithdrawCurrency(_character.Cost);
+            GameManager.UnlockCharacter(_character.Id);
+            OnCharacterSelected();
+        }
+    }
 
-	public void SetCharacter(CharacterData character)
-	{
-		_character = character;
-		CharacterName.Text = _character.Id;
-		if (character.IsUnlocked == false)
-		{
-			BuyButton.Text = "Buy $" + character.Cost;
-		}
-		else
-		{
-			BuyButton.Visible = false;
-		}
-		SetupCharacterButton();
-	}
+    public void SetCharacter(CharacterData character)
+    {
+        _character = character;
+        CharacterName.Text = _character.Id;
+        SetupCharacterButton(character);
+        if (character.IsUnlocked == false)
+        {
+            BuyButton.Text = "Buy $" + character.Cost;
+        }
+        else
+        {
+            BuyButton.Visible = false;
+        }
+    }
 
-	private void SetupCharacterButton()
-	{
-		Texture2D spriteTexture = ResourceLoader.Load<Texture2D>(_character.IconPath);
-		CharacterButton.TextureNormal = spriteTexture;
-		CharacterButton.TextureClickMask = CreateClickMask(spriteTexture);
-		CharacterButton.Modulate = _character.IsUnlocked ? Colors.White : Colors.Gray;
-		CharacterButton.Disabled = !_character.IsUnlocked;
-	}
-
-	private Bitmap CreateClickMask(Texture2D spriteTexture)
-	{
-		// Get the sprite's image
-		Image image = spriteTexture.GetImage();
-		if (image == null)
-		{
-			GD.PrintErr("Failed to get image from texture");
-			return new Bitmap();
-		}
-
-		// Create a BitMap for the click mask
-		Bitmap clickMask = new Bitmap();
-		clickMask.CreateFromImageAlpha(image, 0.1f); // Use alpha threshold to define shape
-
-		return clickMask;
-	}
+    private void SetupCharacterButton(CharacterData character)
+    {
+        Texture2D spriteTexture = ResourceLoader.Load<Texture2D>(_character.IconPath);
+        if (CharacterSprite != null && spriteTexture != null)
+        {
+            CharacterSprite.Texture = spriteTexture;
+        }
+        else
+        {
+            GD.PrintErr($"Failed to load character icon or CharacterSprite is null for character: {_character.Id}");
+        }
+    }
 }
