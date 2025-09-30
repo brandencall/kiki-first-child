4c5993f81eff0e952a0437b74eb8da8cdb0fcbbd
Branden Call <brandencall@live.com>
2025-09-30 14:56:13 -0600

    Moved Velocity component to base enemy

--- scripts/abilities/poison_effect/PoisonEffectAbility.cs
+++ scripts/abilities/poison_effect/PoisonEffectAbility.cs
@@ -3,22 +3,22 @@ using Godot;
 
 public partial class PoisonEffectAbility : Node, IEffect
 {
-    [Export]
-    public int Damage { get; set; } = 1;
-    [Export]
-    public float Duration { get; set; } = 1.5f;
-    [Export]
-    public float TickInterval { get; set; } = 0.5f;
+	[Export]
+	public int Damage { get; set; } = 1;
+	[Export]
+	public float Duration { get; set; } = 1.5f;
+	[Export]
+	public float TickInterval { get; set; } = 0.5f;
 
-    public async Task Apply(BaseEnemy target)
-    {
-        float elapsed = 0f;
+	public async Task Apply(BaseEnemy target)
+	{
+		float elapsed = 0f;
 
-        while (elapsed < Duration)
-        {
-            target.HealthComponent.Damage(Damage);
-            await ToSignal(GetTree().CreateTimer(TickInterval), "timeout");
-            elapsed += TickInterval;
-        }
-    }
+		while (elapsed < Duration)
+		{
+			target.HealthComponent.Damage(Damage);
+			await ToSignal(GetTree().CreateTimer(TickInterval), "timeout");
+			elapsed += TickInterval;
+		}
+	}
 }
