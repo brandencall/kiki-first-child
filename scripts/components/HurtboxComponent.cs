62c41bba509b54e66b1ac9982c8ea18229a0041a
Branden Call <brandencall@live.com>
2025-10-05 07:36:01 -0600

    Fixed test_fire scene

--- scripts/components/HurtboxComponent.cs
+++ scripts/components/HurtboxComponent.cs
@@ -2,37 +2,37 @@ using Godot;
 
 public partial class HurtboxComponent : Area2D
 {
-    [Export]
-    private HealthComponent _healthComponent;
+	[Export]
+	private HealthComponent _healthComponent;
 
-    public Node OwnerEntity { get; set; }
+	public Node OwnerEntity { get; set; }
 
-    public override void _Ready()
-    {
-        CollisionLayer = 32;
-        CollisionMask = 2;
-        Connect("area_entered", new Callable(this, nameof(OnAreaEntered)));
-    }
+	public override void _Ready()
+	{
+		CollisionLayer = 32;
+		CollisionMask = 2;
+		Connect("area_entered", new Callable(this, nameof(OnAreaEntered)));
+	}
 
-    private void OnAreaEntered(Area2D otherArea)
-    {
-        if (otherArea is HitboxComponent hitbox)
-        {
-            DamageContext ctx = new DamageContext
-            {
-                Attacker = hitbox.OwnerEntity,
-                Defender = this.OwnerEntity,
-                BaseDamage = hitbox.Damage,
-                FinalDamage = hitbox.Damage,
-                Effects = hitbox.Effects
-            };
+	private void OnAreaEntered(Area2D otherArea)
+	{
+		if (otherArea is HitboxComponent hitbox)
+		{
+			DamageContext ctx = new DamageContext
+			{
+				Attacker = hitbox.OwnerEntity,
+				Defender = this.OwnerEntity,
+				BaseDamage = hitbox.Damage,
+				FinalDamage = hitbox.Damage,
+				Effects = hitbox.Effects
+			};
 
-            DamageManager.Resolve(ctx);
-        }
-    }
+			DamageManager.Resolve(ctx);
+		}
+	}
 
-    public void DealDamage(float damage)
-    {
-        _healthComponent.Damage(damage);
-    }
+	public void DealDamage(float damage)
+	{
+		_healthComponent.Damage(damage);
+	}
 }
