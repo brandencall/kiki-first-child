9bedd9bb52e3e99d266fee3a9ee1b013bfa97e10
Branden Call <brandencall@live.com>
2025-10-04 18:04:46 -0600

    clean up

--- scripts/components/HurtboxComponent.cs
+++ scripts/components/HurtboxComponent.cs
@@ -2,67 +2,37 @@ using Godot;
 
 public partial class HurtboxComponent : Area2D
 {
-	[Export]
-	private HealthComponent _healthComponent;
+    [Export]
+    private HealthComponent _healthComponent;
 
-	public Node OwnerEntity { get; set; }
+    public Node OwnerEntity { get; set; }
 
-	public override void _Ready()
-	{
-		CollisionLayer = 32;
-		CollisionMask = 2;
-		Connect("area_entered", new Callable(this, nameof(OnAreaEntered)));
-	}
+    public override void _Ready()
+    {
+        CollisionLayer = 32;
+        CollisionMask = 2;
+        Connect("area_entered", new Callable(this, nameof(OnAreaEntered)));
+    }
 
-	private void OnAreaEntered(Area2D otherArea)
-	{
-		if (otherArea is HitboxComponent hitbox)
-		{
-			DamageContext ctx = new DamageContext
-			{
-				Attacker = hitbox.OwnerEntity,
-				Defender = this.OwnerEntity,
-				BaseDamage = hitbox.Damage,
-				FinalDamage = hitbox.Damage,
-				Effects = hitbox.Effects
-			};
+    private void OnAreaEntered(Area2D otherArea)
+    {
+        if (otherArea is HitboxComponent hitbox)
+        {
+            DamageContext ctx = new DamageContext
+            {
+                Attacker = hitbox.OwnerEntity,
+                Defender = this.OwnerEntity,
+                BaseDamage = hitbox.Damage,
+                FinalDamage = hitbox.Damage,
+                Effects = hitbox.Effects
+            };
 
-			DamageManager.Resolve(ctx);
+            DamageManager.Resolve(ctx);
+        }
+    }
 
-			//DealDamage(hitbox.Damage);
-			//GD.Print("HurtboxComponent Owner: " + OwnerEntity);
-			//GD.Print("HitboxComponent Owner: " + hitbox.OwnerEntity);
-
-			//var effects = hitbox.Effects;
-			//// TODO: Should change this to some sort of IEntity so even the Characters can be effected
-			//BaseEnemy enemy = GetParent<BaseEnemy>();
-
-			//if (enemy != null)
-			//{
-			//	foreach (var effect in effects)
-			//	{
-			//		if (effect.IsStateModifier)
-			//		{
-			//			//Fire and forget so that all effects are applied at the same time
-			//			_ = effect.Apply(enemy);
-			//		}
-			//	}
-
-			//	foreach (var effect in effects)
-			//	{
-			//		if (!effect.IsStateModifier)
-			//		{
-			//			//Fire and forget so that all effects are applied at the same time
-			//			_ = effect.Apply(enemy);
-			//		}
-			//	}
-			//}
-		}
-	}
-
-	public void DealDamage(float damage)
-	{
-		_healthComponent.Damage(damage);
-		GD.Print("Damage: " + damage);
-	}
+    public void DealDamage(float damage)
+    {
+        _healthComponent.Damage(damage);
+    }
 }
