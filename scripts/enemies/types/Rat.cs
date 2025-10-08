4e2f2fedb3c794902fb3355a99dde45d98221119
Branden Call <brandencall@live.com>
2025-10-08 09:02:50 -0600

    Poison and Fire particle update

--- scripts/enemies/types/Rat.cs
+++ scripts/enemies/types/Rat.cs
@@ -8,15 +8,15 @@ public partial class Rat : BaseEnemy
 	private PathfindComponent _pathfindComponent;
 	[Export]
 	private HealthComponent _healthComponent;
-    [Export]
-    private EnemyHitbox _hitboxComponent;
+	[Export]
+	private EnemyHitbox _hitboxComponent;
 
 
 	public override void _Ready()
 	{
 		base._Ready();
 		_healthComponent.Died += Die;
-        _hitboxComponent.OwnerEntity = this;
+		_hitboxComponent.OwnerEntity = this;
 	}
 
 	public override void _PhysicsProcess(double delta)
